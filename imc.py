f75e05f63fff412b92096747d350140f83e6985b
EmersonDba84 <emersonpds84@gmail.com>
2024-09-07 20:26:12 -0300

    commit

--- imc.py
+++ imc.py
@@ -1,11 +1,13 @@
+print(input('Qual Seu Nome?'))
+print(input('Qual Sua idade?'))
 peso = float(input('Digite seu Peso: '))
 altura = float(input('Digite Sua Altura: '))
 
 imc = peso / (altura ** 2)
 print('Seu IMC é: ', imc)
 if (imc >= 30):
-    print('Voçe está Obeso!!')
+    print('Você está Obeso!!')
 elif(imc <= 30):
-    print('Seu Indice de massa Corporal está na media')
+    print('Seu Índice de massa Corporal está na media')
 else:
     print('Seu IMC é: ', imc)
